dd5bb3d3f613fca6b67df42b64dfa91f61046d4a
Elizabeth luong <elizabethluong@live.co.uk>
2020-02-10 08:28:33 +0000

    connect backend and frontend

---
+++ flask-backend/static/react/precache-manifest.73e4bc0d32f8166025b1bba3b6a3e846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3dd437f5da24923ebb75c461fd1f5d",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "63e2818588822aa0da02",
+    "url": "/static/react/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "44db4f6cbe10952ca77b",
+    "url": "/static/react/js/2.9a345854.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/react/js/2.9a345854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e2818588822aa0da02",
+    "url": "/static/react/js/main.dfb1a36b.chunk.js"
+  },
+  {
+    "revision": "165304c935f15a4707af",
+    "url": "/static/react/js/runtime-main.7ca781aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
